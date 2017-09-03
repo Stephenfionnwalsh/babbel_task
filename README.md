c638a2aff5110b92f331b3a3b388ff2b163b44df
Stpehenfionnwalsh <stephenfionnwalsh@gmail.com>
2017-09-03 15:39:38 +0200

    added readme file

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 Babbel Bowling Scoreboard Challenge
+
+I went over the three hours for the task so I'll just leave it as it is.
+There are a few things I could to improve it and it is a bit repetitive in places.
+I tend to think of challenge in the simplest possible way and then try to improve it.
+So for the challenge is only applicable to one player but it could be extended to include
+more.
+
+The implementation is pretty simple and I left all logic in the same file.
+It follows a standard bowling score system. Initially, it checks if the first bowl was a
+strike or not.
+If yes then the player has two more bowls and a possible maximum score of thirty.
+If not then the player only has one more bowl and a possible maximum score of ten.
+
+The total score is then calculated using a reduce function.
+
+The score and scores for each round and bowl are rendered to the page.
+
+On Refresh new scores are shown.
--- src/components/scoreboard.js
+++ src/components/scoreboard.js
@@ -4,26 +4,27 @@ import Player from './player';
 const Scoreboard = () => {
     let score = [];
 
+    // First Bowl And Following Bowls If There Is a Strike
     for(let i = 1; i <= 10; i++){
       let firstBowl = Math.floor((Math.random() * 11));
       let secondBowl;
       let thirdBowl;
 
-      console.log(firstBowl);
       if( firstBowl === 10){
         console.log('strike');
         secondBowl = Math.floor((Math.random() * 11));
         thirdBowl = Math.floor((Math.random() * 11));
       }
+
+      // First Bowl And Following Bowls If There Is a No
       if( firstBowl !== 10 ) {
         let remainingPins = 10 - firstBowl;
         let x = Math.floor((Math.random() * remainingPins) + 1)
-        console.log('remainingPins: ' + remainingPins);
         secondBowl = x;
-        if (x === remainingPins){
-          console.log('spare');
-        }
       }
+
+      // If first bowl is strike then push three scores to scores array
+      // else push two scores to array
       if( firstBowl && secondBowl && thirdBowl){
         score.push({ round: i,  firstBowlScore: firstBowl, secondBowlScore: secondBowl, thirdBowlScore: thirdBowl,
                   score: firstBowl + secondBowl + thirdBowl});
@@ -33,22 +34,22 @@ const Scoreboard = () => {
       }
 
     }
-    console.log(score);
-    let bla = score.map((bla) => bla.score);
-    var result = bla.reduce(function(accumulator, currentValue) {
+
+    // Total Score
+    var result = score.map((bla) => bla.score).reduce(function(accumulator, currentValue) {
     return accumulator + currentValue;
     });
-    console.log(result);
-    console.log(bla);
+
     return (
       <div>
         <h1>Bowling Score System : {result}</h1>
-        { score.map((x) => <div style={{ border: "1px solid black", display: "grid", gridTemplateColumns: "1fr 1fr 1fr 1fr" }} key={x.round}>
-                              <h1>Round {x.round}</h1>
-                              <h3>first bowl {x.firstBowlScore}</h3>
-                              <h3>second bowl {x.secondBowlScore}</h3>
+        { score.map((round) => <div style={{ border: "1px solid black", display: "grid", gridTemplateColumns: "1fr 1fr 1fr 1fr 1fr" }} key={round.round}>
+                              <h1>Round {round.round}</h1>
+                              <h3>Total: {round.score}</h3>
+                              <h3>first bowl {round.firstBowlScore}</h3>
+                              <h3>second bowl {round.secondBowlScore}</h3>
                               {
-                                x.thirdBowlScore ? <h3>third bowl {x.thirdBowlScore}</h3> : ''
+                                round.thirdBowlScore ? <h3>third bowl {round.thirdBowlScore}</h3> : ''
                               }
                             </div>)}
       </div>
